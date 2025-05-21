356f94d589030d3e26b5f47d2038702294aa92c3
João Cardoso <jpmsc91@hotmail.com>
2025-05-21 14:33:31 +0100

    Table and Pagination
    Turned columns into table and added pagination

--- src/app/expenses/components/expense-list/expense-list.component.ts
+++ src/app/expenses/components/expense-list/expense-list.component.ts
@@ -1,17 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { AfterViewInit, ViewChild } from '@angular/core';
 import { ExpenseService } from '../../services/expense.service';
-import { ExpenseComponent } from "../expense/expense/expense.component";
 import { CommonModule } from '@angular/common';
 import { combineLatest, map } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [ExpenseComponent, CommonModule],
+  imports: [CommonModule, MatTableModule, MatPaginator, MatPaginatorModule,],
   templateUrl: './expense-list.component.html',
   styleUrl: './expense-list.component.css'
 })
 export class ExpenseListComponent {
+  displayedColumns: string[] = ['demo-position', 'demo-label', 'demo-date', 'demo-value', 'demo-category'];
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
 
   filteredItems$ = combineLatest([
     this.expenseService.expenses$,
@@ -20,7 +26,16 @@ export class ExpenseListComponent {
     map(([expenses, filter]) => this.expenseService.getFilteredItems(expenses, filter))
   );
 
-  constructor (private expenseService:ExpenseService){
+  ngOnInit() {
+    this.filteredItems$.subscribe(data => {
+      this.dataSource.data = data;
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator;
+      }
+    });
+  }
+
+  constructor(private expenseService: ExpenseService) {
     console.log("ExpenseListComponent constructor")
   }
 }
